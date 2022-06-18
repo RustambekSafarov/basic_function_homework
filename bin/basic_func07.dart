aa223e1d2dad252cbc220b9052c2e83ce1085a29
Rustambek <rustambeksafarovv@gmail.com>
2022-06-18 16:04:23 +0500

    Redone

--- bin/basic_func07.dart
+++ bin/basic_func07.dart
@@ -1,6 +1,11 @@
+int func(){
+    return a = 11;
+}
 //  Create a function called func.
 
 //  Create function arguments a.
 
 //  Return the value a.
-void main() {}
+void main() {
+    print(func());
+}
