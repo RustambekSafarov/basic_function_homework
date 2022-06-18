d418b06530224475edd714e6448e74f5b56c95e6
Rustambek <rustambeksafarovv@gmail.com>
2022-06-18 16:14:39 +0500

    Redone

--- bin/basic_func08.dart
+++ bin/basic_func08.dart
@@ -1,6 +1,11 @@
+int func(a){
+    return a+1;
+}
 //  Create a function called func.
 
 //  Create function arguments a.
 
 //  Increase the value of a to one and return.
-void main() {}
+void main() {
+    print(func(4))
+}
